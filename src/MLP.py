585c489307c2ca79744661bd4b54fd35946f92af
Santino Cardelli <sancardelli@gmail.com>
2026-05-13 11:34:05 -0300

    Análisis y Preprocesamiento de Datos

---
+++ src/MLP.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Node:
+    def __init__(self):
+        
+
+class MLP:
+    def __init__(self, n_layers, nodes_per_layer):
+        self.n_layers = n_layers
+        self.nodes_per_layer = nodes_per_layer
+        self.weights = []
